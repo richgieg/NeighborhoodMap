0d54bf8850d9d7e1107c1600a87a2070f626bc08
Richard Gieg <richgieg@users.noreply.github.com>
2016-01-20 20:55:11 -0800

    Implement marker animation

--- src/js/app.js
+++ src/js/app.js
@@ -11,11 +11,29 @@ function SubwayStation(dataObj) {
         map: map,
         title: self.name
     });
+
     self.display = ko.computed(function() {
         return self.name + ' (' + self.line + ' / ' + self.division + ')' +
             ' [' + self.routes.join()  + ']';
     });
+
+    self.toggleMapMarkerBounce = function() {
+        var animationState = self.mapMarker.getAnimation();
+        if (animationState !== null && animationState !== undefined) {
+            self.mapMarker.setAnimation(null);
+        } else {
+            self.mapMarker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    }
+
+    self.focus = function() {
+        map.setCenter({lat: self.latitude, lng: self.longitude});
+        self.toggleMapMarkerBounce();
+    }
+
+    self.mapMarker.addListener('click', self.toggleMapMarkerBounce);
 }
+
 
 function ViewModel() {
     var self = this;
@@ -37,11 +55,6 @@ function ViewModel() {
         return matches;
     });
 
-    // Methods
-    self.activateStation = function(station) {
-        alert(station.display());
-    };
-
     // Initialize the stations array asynchronously
     var jsonUrl = 'https://www.richgieg.com/nyc-subway-api/stations';
     $.getJSON(jsonUrl, function(data) {
@@ -60,7 +73,8 @@ function ViewModel() {
     });
 }
 
-var map;
+
+// Callback that initializes the Google Map object and activates Knockout
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 40.718092, lng: -73.901454},
@@ -70,3 +84,7 @@ function initMap() {
     // Activate Knockout once the map is initialized
     ko.applyBindings(new ViewModel());
 }
+
+
+// Google Map object
+var map;
