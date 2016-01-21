8c53a639b16b6c7db61d206c47cabb7fcf1642c9
Richard Gieg <richgieg@users.noreply.github.com>
2016-01-20 19:07:11 -0800

    Utilize external API for subway location data

--- app.js
+++ app.js
@@ -1,136 +1,29 @@
-function SubwayStation(name, line, division, routes, latitude, longitude) {
+function SubwayStation(dataObj) {
     var self = this;
-    self.name = name;
-    self.line = line;
-    self.division = division;
-    self.routes = routes;
-    self.latitude = latitude;
-    self.longitude = longitude;
+    self.name = dataObj.name;
+    self.line = dataObj.line;
+    self.division = dataObj.division;
+    self.routes = dataObj.routes;
+    self.latitude = dataObj.latitude;
+    self.longitude = dataObj.longitude;
     self.display = ko.computed(function() {
         return self.name + ' (' + self.line + ' / ' + self.division + ')' +
             ' [' + self.routes.join()  + ']';
     });
 }
 
-
-function createSubwayStationArray(records) {
-    // Sort by latitude, then by longitude
-    records.sort(function(a, b) {
-        // Latitude first
-        if (a.Station_Latitude < b.Station_Latitude) {
-            return -1;
-        } else if (a.Station_Latitude > b.Station_Latitude) {
-            return 1;
-        // Longitude second
-        } else if (a.Station_Longitude < b.Station_Longitude) {
-            return -1;
-        } else if (a.Station_Longitude > b.Station_Longitude) {
-            return 1;
-        // Equal
-        } else {
-            return 0;
-        }
-    });
-
-    // Filter duplicate records (based on latitude and longitude)
-    var curLat, curLong;
-    var prevLat = 0;
-    var prevLong = 0;
-    var dupesRemoved = [];
-    for (var i = 0; i < records.length; i++) {
-        curLat = records[i].Station_Latitude;
-        curLong = records[i].Station_Longitude;
-        if ((curLat !== prevLat) || (curLong !== prevLong)) {
-            dupesRemoved.push(records[i]);
-            prevLat = curLat;
-            prevLong = curLong;
-        }
-    }
-
-    // Build list of route property names for next step
-    var routeProperties = [];
-    for (var i = 1; i < 12; i++) {
-        routeProperties.push('Route_' + i);
-    }
-
-    // Convert records to array of SubwayStation objects
-    var locations = [];
-    var routes;
-    var curRecord;
-    for (var i = 0; i < dupesRemoved.length; i++) {
-        curRecord = dupesRemoved[i];
-
-        // Parse routes
-        routes = [];
-        for (var j = 0; j < 11; j++) {
-            if (curRecord[routeProperties[j]]) {
-                routes.push(curRecord[routeProperties[j]]);
-            }
-        }
-        routes.sort();
-
-        // Create SubwayStation object and push to locations array
-        locations.push(
-            new SubwayStation(
-                curRecord.Station_Name,
-                curRecord.Line,
-                curRecord.Division,
-                routes,
-                curRecord.Station_Latitude,
-                curRecord.Station_Longitude
-        ));
-    }
-
-    // Sort by station name, then by line
-    locations.sort(function(a, b) {
-        // Name first
-        if (a.name < b.name) {
-            return -1;
-        } else if (a.name > b.name) {
-            return 1;
-        // Line second
-        } else if (a.line < b.line) {
-            return -1;
-        } else if (a.line > b.line) {
-            return 1;
-        // Equal
-        } else {
-            return 0;
-        }
-    });
-
-    // Filter duplicate stations (based on name and line)
-    var locsToFilter = locations;
-    locations = [];
-    var curName, curLine;
-    var prevName = '';
-    var prevLine = '';
-    for (var i = 0; i < locsToFilter.length; i++) {
-        curName = locsToFilter[i].name;
-        curLine = locsToFilter[i].line;
-        if ((curName != prevName) || (curLine != prevLine)) {
-            locations.push(locsToFilter[i]);
-            prevName = curName;
-            prevLine = curLine;
-        }
-    }
-
-    return locations;
-}
-
-
 function ViewModel() {
     var self = this;
 
     // Observables
-    self.locations = ko.observableArray([]);
+    self.stations = ko.observableArray([]);
     self.filter = ko.observable('');
 
     // Computed Observables
-    self.matchingLocations = ko.computed(function() {
+    self.matchingStations = ko.computed(function() {
         var matches = [];
         var re = new RegExp(self.filter(), 'i');
-        self.locations().forEach(function(val) {
+        self.stations().forEach(function(val) {
             if (val.name.search(re) !== -1) {
                 matches.push(val);
             }
@@ -139,21 +32,22 @@ function ViewModel() {
     });
 
     // Methods
-    self.talk = function(location) {
-        alert(location);
+    self.focus = function(station) {
+        alert(station.display());
     };
 
-    // Initialize the locations array (asynchronously)
-    Papa.parse('nyc-subway-stations.csv', {
-        header: true,
-        download: true,
-        complete: function(results) {
-            // Process the CSV records to create array of SubwayStation objects
-            self.locations(createSubwayStationArray(results.data));
-        }
+    // Initialize the stations array asynchronously
+    var jsonUrl = 'https://www.richgieg.com/nyc-subway-api/stations';
+    $.getJSON(jsonUrl, function(data) {
+        var stations = [];
+        data.stations.forEach(function(dataObj) {
+            stations.push(new SubwayStation(dataObj));
+        });
+        self.stations(stations);
+    }).fail(function() {
+        console.log('ERROR: Could not acquire subway station data');
     });
 }
-
 
 // Activate Knockout
 ko.applyBindings(new ViewModel());
